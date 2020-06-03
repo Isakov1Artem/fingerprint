894f8286c1109a996363fc2b3d8a522861fe4be4
Isakov1Artem <59707812+Isakov1Artem@users.noreply.github.com>
2020-06-03 19:52:03 +0300

    Add files via upload

---
+++ TestApp/views.py
@@ -0,0 +1,118 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.http import Http404
+from BiometricAuth.models import (
+    UserBiometry,
+    IrisImages,
+    FingerPrintImages,
+)
+from BiometricAuth.forms import (
+    IrisImagesFormset,
+    IrisImagesForm,
+    FingerPrintImagesFormset,
+    FingerPrintImagesForm,
+)
+
+class IndexPage(View):
+    def get(self, request):
+        context = {
+
+        }
+        return render(request, 'TestApp/index.html', context=context)
+
+class PersonalAccount(View):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            raise Http404
+        user_biometry = UserBiometry.objects.get(user=request.user.id)
+
+        formsets = {
+            'iris_formset':IrisImagesFormset(instance=user_biometry, prefix='iris_image'),
+            'fingerprint_formset':FingerPrintImagesFormset(instance=user_biometry, prefix='fingerprint_image'),
+
+        }
+
+
+        context = {
+            'formsets' : formsets
+        }
+        # context = {
+        #     'iris_formset' : iris_formset
+        # }
+        return render(request, 'TestApp/personal_account.html', context=context)
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            raise Http404
+
+        user_biometry = get_object_or_404(UserBiometry, user=request.user.id)
+        formsets = {
+            'iris_formset':IrisImagesFormset(instance=user_biometry, prefix='iris_image'),
+            'fingerprint_formset':FingerPrintImagesFormset(instance=user_biometry, prefix='fingerprint_image'),
+
+        }
+        form_type = request.POST.get('form_type').strip()
+        print('Form type={}'.format(form_type))
+        if form_type == 'iris':
+            iris_formset = IrisImagesFormset(
+                request.POST or None,
+                request.FILES or None,
+                prefix='iris_image',
+                instance = user_biometry
+                )
+            if iris_formset.is_valid():
+                for form in iris_formset:
+                    iris_image = form.cleaned_data.get('iris_image')
+                    # Обновляем счетчик для каждой измененной формы
+                    if form.has_changed():
+                        if form.cleaned_data.get('DELETE'):
+                            user_biometry.change_iris_photo_counter(increase = False)
+                            user_biometry.save()
+                        elif iris_image :
+
+                            user_biometry.change_iris_photo_counter()
+                            user_biometry.save()
+
+                print('Счетчик: ', user_biometry.iris_photo_counter)
+                iris_formset.save()
+
+                return redirect('personal-account')
+            else:
+                # Чтобы отобразить ошибки
+                formsets['iris_formset'] = iris_formset
+
+        elif form_type == 'fingerprint':
+            fingerprint_formset = FingerPrintImagesFormset(
+                request.POST or None,
+                request.FILES or None,
+                prefix='fingerprint_image',
+                instance = user_biometry
+                )
+            if fingerprint_formset.is_valid():
+                for form in fingerprint_formset:
+                    fingerprint_image = form.cleaned_data.get('fingerprint_image')
+                    # Обновляем счетчик для каждой измененной формы
+                    if form.has_changed():
+                        if form.cleaned_data.get('DELETE'):
+                            user_biometry.change_finger_photo_counter(increase = False)
+                            user_biometry.save()
+                        elif fingerprint_image :
+
+                            user_biometry.change_finger_photo_counter()
+                            user_biometry.save()
+
+                print('Счетчик: ', user_biometry.fingerprint_photo_counter)
+                fingerprint_formset.save()
+
+                return redirect('personal-account')
+            else:
+                # Чтобы отобразить ошибки
+                formsets['fingerprint_formset'] = fingerprint_formset
+        elif form_type == 'face':
+            pass
+
+        context = {
+            'formsets' : formsets,
+
+        }
+        return render(request, 'TestApp/personal_account.html', context=context)
