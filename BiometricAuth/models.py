a016275b157f4c18565fdc9cce219189ff300647
Isakov1Artem <59707812+Isakov1Artem@users.noreply.github.com>
2020-06-03 19:51:01 +0300

    Add files via upload

---
+++ BiometricAuth/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import User, UserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .iris_auth.enroll_single import enroll_single
+from .src.fp_join_folder import fp_join_folder
+
+#----------------------------- Общие классы
+class UserBiometry(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    iris_photo_counter = models.PositiveSmallIntegerField(default=0)
+    face_photo_counter = models.PositiveSmallIntegerField(default=0)
+    fingerprint_photo_counter = models.PositiveSmallIntegerField(default=0)
+
+    def change_iris_photo_counter(self, increase=True):
+        if increase:
+            self.iris_photo_counter += 1
+        else:
+            self.iris_photo_counter -= 1
+
+    def change_finger_photo_counter(self, increase=True):
+        if increase:
+            self.fingerprint_photo_counter += 1
+        else:
+            self.fingerprint_photo_counter -= 1
+
+    def __str__(self):
+        return 'user - {0}'.format(self.user)
+
+# Автоматическое создание/изменение модели UserBiometry при создании/изменении модели пользователя
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserBiometry.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userbiometry.save()
+
+
+
+
+
+# --------------------------- Модели для алгоритмов:
+def iris_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'biometric_data/user_{0}/iris/{1}'.format(instance.user.id, filename)
+
+def fingerprint_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'biometric_data/user_{0}/fingerprint/{1}'.format(instance.user.id, filename)
+
+class IrisImages(models.Model):
+    user = models.ForeignKey(UserBiometry, on_delete=models.CASCADE,related_name='iris_image')
+    iris_image = models.ImageField(upload_to=iris_directory_path)
+
+    def save(self, *args, **kwargs):
+        # ---------- Second realisation
+        super().save(*args, **kwargs)
+        enroll_single(image = self.iris_image, user_id = self.user.id)
+
+
+class FaceImages(models.Model):
+    pass
+
+class FingerPrintImages(models.Model):
+    user = models.ForeignKey(UserBiometry, on_delete=models.CASCADE,related_name='fingerprint_image')
+    fingerprint_image = models.ImageField(upload_to=fingerprint_directory_path)
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        fp_join_folder(image = self.fingerprint_image, user_id = self.user.id)
---
+++ BiometricAuth/utils.py
@@ -0,0 +1,24 @@
+import os
+from django.conf import settings
+
+def check_and_create_folder(out_folder):
+    if os.path.exists(out_folder):
+        if os.path.isdir(out_folder):
+            print('Каталог найден')
+            # print('Список объектов в нем: ',os.listdir(out_folder))
+    else:
+        print ('Объект не найден')
+        try:
+            os.mkdir(out_folder)
+        except OSError:
+            print ("Creation of the directory %s failed" % out_folder)
+        else:
+            print ("Successfully created the directory %s " % out_folder)
+
+def get_iris_mat_path(user_biometry_id):
+    user_iris_folder_name = 'biometric_data/user_{}/iris/mat/'.format(user_biometry_id)
+    return os.path.join(settings.MEDIA_ROOT, user_iris_folder_name)
+
+def get_fingerprint_skelet_path(user_biometry_id):
+    user_fingerprint_folder_name = 'biometric_data\\user_{}\\fingerprint\\skelet\\'.format(user_biometry_id)
+    return os.path.join(settings.MEDIA_ROOT, user_fingerprint_folder_name)
